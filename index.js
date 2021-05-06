87ebca10038f2dfa24d6784b05a8fcab7a731514
Sherapat <st.rejepova@gmail.com>
2021-05-06 03:16:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return "I can't hear you!";
-  if (string.toLowerCase) {
+  if (string.toLowerCase()) {
   }
   return "YES INDEED!";
   if (string.toUpperCase()) {
